17ee0ac23ac571e3823baa0185d698983523b304
Jishnu Dantu <jishnudantu@Jishnus-MacBook-Pro.local>
2019-08-25 09:59:09 -0700

    updated sourse.js

--- basics/scope.js
+++ basics/scope.js
@@ -1,9 +1,30 @@
-let var1 = 'varOne'
+
+// Javascript uses Lexical scoping (Static Scope)
+// 2 types of scopes ->
+//Global scope : Defined outside all code blocks
+//Local scope : Defined inside a code block
+
+// In a scope you can access variables defined in that scope, or in any parent/ancestor
+
+//Global scope (var1)
+  //Local scope(var2)
+    //Local scope(var4)
+  //Local scope(var3)
+
+
+let var1 = 'varOne' // this is a global scope
 
 if(true){
     console.log(var1)
-    let var2 = 'varTwo'
+    let var2 = 'varTwo'// this is a local scope
     console.log(var2)
+
+    if (true){
+        let var4 = 'varFour'
+    }
+}
+if(true){
+    let var3 = 'varThree'
 }
 
-console.log(var2)
+console.log(var2)// out of scope
